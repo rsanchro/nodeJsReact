7180ea94902a0c59c786a5a9feb7eafd88beabfb
tr08.wfsd24.ene19 <tr08.wfsd24.ene19@bootcamp-upgrade.com>
2019-06-30 01:58:04 +0200

    Fix mongoose warning deprecation: change to useFindAndModify = false en config global

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import router from './routes'; // importamos las rutas rutes (index.js)
 
 // Conexión a la base de datos y nombre de bd
 mongoose.Promise=global.Promise;
+mongoose.set('useFindAndModify', false);
 const dbUrl = 'mongodb://localhost:27017/dbsistema';
 mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
 .then(mongoose => console.log('Conectando a la base de datos en el puerto 27017')) // si todo es correcto msg
